771905d07a397742594cbd7c2df17f4b50c19d6b
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-01-31 13:36:57 +0100

    booking controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,2 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
-  def search
-    @users = User.near(params["user"]["address"], 20)
-    set_hash(@users)
-    render :index
-  end
-
-
-  def set_hash(users)
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
-      # marker.infowindow render_to_string(partial: "/users/map_box", locals: { user: user })
-    end
-  end
-
 end
