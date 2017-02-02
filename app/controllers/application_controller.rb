71735065eca393f7c7d01b8c6da8218f0a8a141c
Quan Trinh <trinhquan1123@yahoo.com>
2017-02-02 08:03:11 +0100

    ajax-ified elastic-search for email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   protect_from_forgery
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'pages#landing_page'
+
   devise_for :users
 
   devise_scope :user do
     get '/users/:id/show', to: "devise/registrations#show"
   end
 
-  root to: 'pages#landing_page'
-  get '/home', to: "pages#home"
+  resources :users, only: :index do
+    resources :bookings
+  end
 
-  resources :bookings
+  get 'home', to: "pages#home"
+  get 'search', to: "search#search"
+
 end
