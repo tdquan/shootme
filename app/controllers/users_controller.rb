eeff76ae60a10bf7945b29b01ea2abe5be302831
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-02-27 11:18:44 +0100

    inbox page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
   end
 
   def inbox
+    @conversations = Conversation.all
     # @bookings = current_user.bookings
     @requests_to_self = current_user.requests_to_self
     @requests_to_others = current_user.requests_to_others
-    @conversations_client = Conversation.find_by client_id: current_user.id
-    @conversations_user = Conversation.find_by user_id: current_user.id
-    @messages_user = @conversations_user.messages
-    @messages_client = @conversations_client.messages
+    # @conversations_client = Conversation.find_by client_id: current_user.id
+    # @conversations_user = Conversation.find_by user_id: current_user.id
+    # @messages_user = @conversations_user.messages
+    # @messages_client = @conversations_client.messages
   end
 
   private
