cb708f2ce47e0d688c0a51a131bc3c9d6862401f
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-03-14 15:21:16 +0100

    autocomplete implemented for address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313142936) do
+ActiveRecord::Schema.define(version: 20170314103609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,8 @@ ActiveRecord::Schema.define(version: 20170313142936) do
     t.string   "role"
     t.boolean  "pro"
     t.integer  "fee_cents",              default: 0,  null: false
+    t.string   "city"
+    t.string   "country"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
