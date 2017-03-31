f0ccecb3608141f7439e06e74e91b2dc587f2656
Quan Trinh <trinhquan1123@yahoo.com>
2017-03-31 17:11:38 +0200

    fixed search parameters, users now can have multiple roles

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,10 @@ class SearchController < ApplicationController
     @users = []
     if params[:user][:q].blank? && params[:user][:location].blank? && params[:user][:profession].count <= 1
       @users = User.all
+    elsif params[:user][:q].present? && params[:user][:location].present? && params[:user][:profession].count > 1
+      @users = User.query_search(params[:user][:q])
+      @users = @users.select { |user| (user.address && user.address.include?(params[:user][:location])) }
+      @users = @users.select { |user| !(user.role.split(" - ") & params[:user][:profession]).empty? }
     elsif params[:user][:q].present? && params[:user][:location].blank? && params[:user][:profession].count <= 1
       @users = User.query_search(params[:user][:q])
     elsif params[:user][:q].present? && params[:user][:location].present? && params[:user][:profession].count <= 1
@@ -12,12 +16,12 @@ class SearchController < ApplicationController
       @users = @users.select { |user| (user.address && user.address.include?(params[:user][:location])) }
     elsif params[:user][:q].present? && params[:user][:location].blank? && params[:user][:profession].count > 1
       @users = User.query_search(params[:user][:q])
-      @users = @users.select { |user| (user.role.split("-") - params[:user][:profession]).empty? }
+      @users = @users.select { |user| !(user.role.split(" - ") & params[:user][:profession]).empty? }
     elsif params[:user][:q].blank? && params[:user][:location].present? && params[:user][:profession].count <= 1
       @users = User.search_by_location(params[:user][:location])
     elsif params[:user][:q].blank? && params[:user][:location].present? && params[:user][:profession].count > 1
       @users = User.search_by_location(params[:user][:location])
-      @users = @users.select { |user| (user.role.split("-") - params[:user][:profession]).empty? }
+      @users = @users.select { |user| !(user.role.split(" - ") & params[:user][:profession]).empty? }
     elsif params[:user][:q].blank? && params[:user][:location].blank? && params[:user][:profession].count > 1
       @users = User.search_by_profession(params[:user][:profession].join(" "))
     end
