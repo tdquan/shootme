eda69a6573252718e744138803d65a7e6d8215fd
Quan Trinh <trinhquan1123@yahoo.com>
2017-04-05 16:33:48 +0200

    quick fix for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,11 +56,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => 'shootme.cbm@gmail.com',
-   :password             => 'ShootMeCBMWEB',
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'shootme.cbm@gmail.com',
+    :password             => 'ShootMeCBMWEB',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 end
