021d1ad1299711dc7be3042c8c2068ed37a668fb
Quan Trinh <trinhquan1123@yahoo.com>
2017-04-12 11:05:50 +0200

    able to build markers based on search result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@
 
 class SearchController < ApplicationController
   def search
+    # Search filtering
     @users = []
     if params[:user][:q].blank? && params[:user][:location].blank? && params[:user][:profession].count <= 1
       @users = User.all
@@ -24,6 +25,20 @@ class SearchController < ApplicationController
       @users = @users.select { |user| !(user.role.split(" - ") & params[:user][:profession]).empty? }
     elsif params[:user][:q].blank? && params[:user][:location].blank? && params[:user][:profession].count > 1
       @users = User.search_by_profession(params[:user][:profession].join(" "))
+    end
+
+    # Get user with location
+    @user_markers = []
+    @users.each do |user|
+      if user.latitude != nil && user.longitude != nil
+        @user_markers << user
+      end
+    end
+
+    @hash = Gmaps4rails.build_markers(@user_markers) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow render_to_string(partial: "/users/map_box", locals: { user: user })
     end
 
     respond_to do |f|
