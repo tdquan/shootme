86200f4861ebafa739234a41e6c64e36b60599c3
Quan Trinh <trinhquan1123@yahoo.com>
2017-05-30 12:31:06 +0200

    fixed merge conflicts

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -2,4 +2,7 @@ $(document).on('ready', function(){
   $(".alert").fadeTo(2000, 500).slideUp(500, function(){
     $(".alert").slideUp(500);
   });
+  $(".notice").fadeTo(2000, 500).slideUp(500, function(){
+    $(".notice").slideUp(500);
+  });
 });
